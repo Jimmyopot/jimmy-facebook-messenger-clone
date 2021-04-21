7763c26281ff3ad3f494de72c3fe8a00270b6f9d
Jimmyopot <jimmyopot@gmail.com>
2021-04-21 11:48:14 +0200

    Added layers of security features

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,16 @@
 import firebase from 'firebase';
+import dotenv from 'dotenv';
+ 
+dotenv.config()
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCZpCUgWcqQTMbWzKU-M4bfxDw_l8SLLpc",
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "jimmy-fb-clone.firebaseapp.com",
     projectId: "jimmy-fb-clone",
     storageBucket: "jimmy-fb-clone.appspot.com",
-    messagingSenderId: "888852774712",
-    appId: "1:888852774712:web:c66ddee0f943aa481bcbcd",
-    measurementId: "G-E5WT1LFYC7"
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
   });
 
   const db = firebaseApp.firestore();
